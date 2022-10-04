7963ef773d080c9319a0c9a44f08e967e5f58daa
ziauddin-sayyed <sayyedzia5@gmail.com>
2022-10-04 19:42:28 +0530

    Readme

--- index.js
+++ index.js
@@ -140,16 +140,22 @@ counters.forEach(counter => {
 function myMap() {
   var mapStyle = [{elementType:"geometry",stylers:[{color:"#f5f5f5"}]},{elementType:"labels",stylers:[{visibility:"on"}]},{elementType:"labels.icon",stylers:[{visibility:"off"}]},{elementType:"labels.text.fill",stylers:[{color:"#616161"}]},{elementType:"labels.text.stroke",stylers:[{color:"#f5f5f5"}]},{featureType:"administrative",elementType:"geometry",stylers:[{visibility:"off"}]},{featureType:"administrative.land_parcel",stylers:[{visibility:"off"}]},{featureType:"administrative.land_parcel",elementType:"labels.text.fill",stylers:[{color:"#bdbdbd"}]},{featureType:"administrative.neighborhood",stylers:[{visibility:"off"}]},{featureType:"landscape",elementType:"geometry.fill",stylers:[{color:"#f2f5f7"}]},{featureType:"poi",stylers:[{visibility:"off"}]},{featureType:"poi",elementType:"geometry",stylers:[{color:"#eeeeee"}]},{featureType:"poi",elementType:"labels.text.fill",stylers:[{color:"#757575"}]},{featureType:"poi.park",elementType:"geometry",stylers:[{color:"#e5e5e5"}]},{featureType:"poi.park",elementType:"labels.text.fill",stylers:[{color:"#9e9e9e"}]},{featureType:"road",elementType:"geometry",stylers:[{color:"#ffffff"}]},{featureType:"road",elementType:"geometry.fill",stylers:[{color:"#d9e3e8"}]},{featureType:"road",elementType:"geometry.stroke",stylers:[{color:"#d9e3e8"}]},{featureType:"road",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"road.arterial",elementType:"labels.text.fill",stylers:[{color:"#757575"}]},{featureType:"road.highway",elementType:"geometry",stylers:[{color:"#dadada"}]},{featureType:"road.highway",elementType:"labels.text.fill",stylers:[{color:"#616161"}]},{featureType:"road.local",elementType:"labels.text.fill",stylers:[{color:"#9e9e9e"}]},{featureType:"transit",stylers:[{visibility:"off"}]},{featureType:"transit.line",elementType:"geometry",stylers:[{color:"#e5e5e5"}]},{featureType:"transit.station",elementType:"geometry",stylers:[{color:"#eeeeee"}]},{featureType:"water",elementType:"geometry",stylers:[{color:"#c9c9c9"}]},{featureType:"water",elementType:"geometry.fill",stylers:[{color:"#e6eaea"}]},{featureType:"water",elementType:"labels.text.fill",stylers:[{color:"#9e9e9e"}]}];
   var loctn_filt = [[20.5937,78.9629],[51.3781,-1.4360],[37.0902,125.7129],[35.8617,-104.1954]];
+
+  var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+  var mapZoom = vw > 900 ? 2 : 1;
+  // console.log(vw);
+
   var mapProp= {
       center:new google.maps.LatLng(0,0),
-      zoom:2,styles:mapStyle,
+      zoom:mapZoom,styles:mapStyle,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
   var infowindow = new google.maps.InfoWindow();
   var marker, i;
   for (i = 0; i < loctn_filt.length; i++) {
-    console.log(loctn_filt[i][0]);
+    // console.log(loctn_filt[i][0]);
       marker = new google.maps.Marker({
           position: new google.maps.LatLng(loctn_filt[i][0], loctn_filt[i][1]),
           map: map,icon:"https://storage.googleapis.com/kido-assets/mapMarker.svg"
